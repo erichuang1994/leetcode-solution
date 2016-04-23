ea5c962ff58d885ac48eb80b54839d9fd37216f2
eric <q465168867@gmail.com>
2016-04-23 13:14:41 +0800

    discuss里有好的多的解法

--- 236.Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
+++ 236.Lowest_Common_Ancestor_of_a_Binary_Tree.cpp
@@ -78,6 +78,16 @@ public:
         return false;
     }
 };
+// 更快而且更简洁的方法
+// class Solution {
+// public:
+//     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+//         if (root == p || root == q || root == NULL) return root;
+//         TreeNode *left = lowestCommonAncestor(root->left, p, q)l
+//		   TreeNode*right = lowestCommonAncestor(root->right, p, q);
+//         return left && right ? root : left ? left : right;
+//     }
+// };
 int main(){
 	TreeNode root(0);
 	TreeNode left(10);
