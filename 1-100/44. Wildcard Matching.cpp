94e7f350cb4d2534deaf931aa658221242e3ef24
eric <q465168867@gmail.com>
2016-09-03 20:09:43 +0800

    add 44 Wildcard Matching

---
+++ 1-100/44. Wildcard Matching.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+  bool isMatch(string s, string p) {
+    int lens = s.length();
+    int lenp = p.length();
+    if (p.empty() && !s.empty()) {
+      return false;
+    }
+    if (s.empty()) {
+      for (int i = 0; i < lenp; i++) {
+        if (p[i] != '*') {
+          return false;
+        }
+      }
+      return true;
+    }
+    int dp[lens + 1];
+    if (p[0] == '*') {
+      fill_n(dp, lens + 1, 1);
+    } else if (p[0] == '?') {
+      fill_n(dp, lens + 1, 0);
+      dp[1] = 1;
+    } else {
+      fill_n(dp, lens + 1, 0);
+      dp[1] = p[0] == s[0] ? 1 : 0;
+    }
+    for (int i = 1; i < lenp; i++) {
+      if (p[i] == '*') {
+        bool flag = dp[0];
+        for (int j = 0; j <= lens; j++) {
+          flag = flag | dp[j];
+          dp[j] = flag;
+        }
+      } else if (p[i] == '?') {
+        for (int j = lens; j > 0; j--) {
+          dp[j] = dp[j - 1] == 1 ? 1 : 0;
+        }
+        dp[0] = 0;
+      } else {
+        for (int j = lens; j > 0; j--) {
+          dp[j] = dp[j - 1] == 1 && p[i] == s[j - 1] ? 1 : 0;
+        }
+        dp[0] = 0;
+      }
+    }
+    return dp[lens];
+  }
+};
