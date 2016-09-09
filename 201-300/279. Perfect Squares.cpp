7034e87320876e6c20f02777ce04b8c84fb9f37e
eric <q465168867@gmail.com>
2016-09-09 10:18:52 +0800

    use static vari

--- 201-300/279. Perfect Squares.cpp
+++ 201-300/279. Perfect Squares.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
   int numSquares(int n) {
-    vector<int> res(n + 1, 0);
+    static vector<int> res(1, 0);
     int tmp;
-    for (int i = 1; i <= n; i++) {
-      if (pow(int(pow(i, 0.5)), 2) == i) {
-        res[i] = 1;
-        continue;
+    if (res.size() <= n) {
+      for (int i = res.size(); i <= n; i++) {
+        if (pow(int(pow(i, 0.5)), 2) == i) {
+          res.push_back(1);
+          continue;
+        }
+        tmp = -1;
+        for (int j = 1; j * j <= i; j++) {
+          tmp = tmp == -1 ? res[i - j * j] + 1 : min(tmp, res[i - j * j] + 1);
+        }
+        res.push_back(tmp);
       }
-      tmp = -1;
-      for (int j = 1; j * j <= i; j++) {
-        tmp = tmp == -1 ? res[i - j * j] + 1 : min(tmp, res[i - j * j] + 1);
-      }
-      res[i] = tmp;
     }
     return res[n];
   }
