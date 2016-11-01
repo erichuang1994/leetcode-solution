edd452201978b5a64608b94116a1d32f0839f3f1
eric <q465168867@gmail.com>
2016-11-01 19:02:39 +0800

    simplify code

--- 1-100/41. First Missing Positive.cpp
+++ 1-100/41. First Missing Positive.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
   int firstMissingPositive(vector<int> &nums) {
-    if (nums.size() == 0)
-      return 1;
-    for (int i = 0; i < nums.size(); ++i) {
+    for (int i = 0; i < nums.size(); ++i)
       while (0 < nums[i] && nums[i] <= nums.size() &&
-             nums[i] != nums[nums[i] - 1]) {
+             nums[i] != nums[nums[i] - 1])
         swap(nums[i], nums[nums[i] - 1]);
-      }
-    }
-    for (int i = 0; i < nums.size(); ++i) {
+    for (int i = 0; i < nums.size(); ++i)
       if (nums[i] != i + 1)
         return i + 1;
-    }
     return nums.size() + 1;
   }
 };
