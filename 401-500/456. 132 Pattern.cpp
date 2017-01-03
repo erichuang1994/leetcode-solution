a8e9554b4774a51d77747c7c5f3419d84df11eb0
eric <q465168867@gmail.com>
2017-01-03 21:17:34 +0800

    update 456. 132 Pattern.cpp

--- 401-500/456. 132 Pattern.cpp
+++ 401-500/456. 132 Pattern.cpp
@@ -7,7 +7,7 @@ public:
     min = max = nums[0];
     vec.emplace_back(min, max);
     for (int i = 1; i < nums.size(); ++i) {
-      if(nums[i]<vec.back().second&&nums[i]>vec.back().first)return true;
+      if((nums[i]<vec.back().second&&nums[i]>vec.back().first)||(nums[i] > min && nums[i] < max))return true;
       if (nums[i] < min) {
         auto top = vec.back();
         if (max < top.second) {
@@ -17,20 +17,32 @@ public:
       } else if (nums[i] > max) {
         max = nums[i];
         if (max > vec.back().second) {
-          vec.back().second = max;
-          vec.back().first = min;
-          while (vec.size() > 1 &&
-                 vec[vec.size() - 2].second < vec.back().second) {
-            vec[vec.size() - 2].second = vec.back().second;
-            vec[vec.size() - 2].first = vec.back().first;
+          while(!vec.empty()&&vec.back().second < max)
             vec.pop_back();
-          }
+          vec.emplace_back(min, max);
           if(vec.size()>1&&vec[vec.size() - 2].first < vec.back().second)return true;
         }
-      } else if (nums[i] > min && nums[i] < max) {
-        return true;
       }
     }
     return false;
   }
 };
+// 代码更短的版本
+// class Solution {
+// public:
+//   bool find132pattern(vector<int> &nums) {
+//     stack<int> s;
+//     int min = INT_MIN;
+//     for(int i = nums.size()-1;i>=0;--i){
+//       if(nums[i]<min)return true;
+//       else {
+//         while(!s.empty()&&s.top()<nums[i]){
+//           min = s.top();
+//           s.pop();
+//         }
+//         s.push(nums[i]);
+//       }
+//     }
+//     return false;
+//   }
+// };
